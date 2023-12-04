05e5b00fe5c266567251af1ff6b2a9cdcddb62bf
Mohamed Bouda <126961762+boudaMed75@users.noreply.github.com>
2023-12-04 13:51:43 +0100

    Update Turc Flag.py

--- flags/Turc Flag.py
+++ flags/Turc Flag.py
@@ -5,14 +5,6 @@ width = 1200
 height = 800
 
 
-
-
-
-
-# Dessiner l'étoile
-star_color = (255, 255, 255)
-star_x = width // 2
-star_y = height // 2
 star_points = [
     (star_x, star_y - star_radius),
     (star_x + 0.5878 * star_radius, star_y + 0.809 * star_radius),
