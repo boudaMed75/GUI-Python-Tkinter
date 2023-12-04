f0d14df22b631702817ebd2aa1e85ffaf30df8d3
Mohamed Bouda <126961762+boudaMed75@users.noreply.github.com>
2023-12-04 13:51:51 +0100

    Update Turc Flag.py

--- flags/Turc Flag.py
+++ flags/Turc Flag.py
@@ -5,13 +5,6 @@ width = 1200
 height = 800
 
 
-star_points = [
-    (star_x, star_y - star_radius),
-    (star_x + 0.5878 * star_radius, star_y + 0.809 * star_radius),
-    (star_x - 0.9511 * star_radius, star_y + 0.309 * star_radius),
-    (star_x + 0.9511 * star_radius, star_y + 0.309 * star_radius),
-    (star_x - 0.5878 * star_radius, star_y + 0.809 * star_radius)
-]
 draw.polygon(star_points, fill=star_color)
 
 # Afficher le drapeau de la Turquie
